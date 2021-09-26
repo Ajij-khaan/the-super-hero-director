607079e91a360bfe0e71af9548630fdbcc01e474
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-09-26 21:34:07 +0600

    Cart Update

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,6 +3,7 @@ import Fish from '../Fish/Fish';
 
 const Body = () => {
     const [fishes, setFishes] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('./fakeData.JSON')
@@ -10,19 +11,37 @@ const Body = () => {
             .then(data => setFishes(data))
     }, [])
 
+
+    const handleMyCart = (fish) => {
+        // console.log(fish);
+        const newCart = [...cart, fish];
+        setCart(newCart);
+    }
+
     return (
         <div className="container mt-5">
             <div className="row">
                 <div className="col-md-9">
                     <div className="row row-cols-1 row-cols-md-3 g-4">
                         {
-                            fishes.map(fish => <Fish key={fish.id} fish={fish} ></Fish>)
+                            fishes.map(fish => <Fish
+                                key={fish.id}
+                                fish={fish}
+                                handleMyCart={handleMyCart}
+                            >
+                            </Fish>)
                         }
                     </div>
                 </div>
                 <div className="col-md-3">
-                    <h4>Fish Added: </h4>
-                    <h4>Total Cost: </h4>
+                    <div className="card">
+                        <div className="card-header fw-bold"> My Cart </div>
+                        <div className="card-body">
+                            <h5 className="text-start">Totad Fish: {cart.length}</h5>
+                            <h5 className="text-start">Total COst: </h5>
+
+                        </div>
+                    </div>
                 </div>
 
             </div>
