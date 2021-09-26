82ec3313c30a16d06d62c0c7e20bf1cf11292bae
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-09-26 20:50:21 +0600

    design

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -11,8 +11,7 @@ const Body = () => {
     }, [])
 
     return (
-        <div className="container">
-
+        <div className="container mt-5">
             <div className="row">
                 <div className="col-md-9">
                     <div className="row row-cols-1 row-cols-md-3 g-4">
@@ -22,6 +21,8 @@ const Body = () => {
                     </div>
                 </div>
                 <div className="col-md-3">
+                    <h4>Fish Added: </h4>
+                    <h4>Total Cost: </h4>
                 </div>
 
             </div>
--- src/components/Fish/Fish.js
+++ src/components/Fish/Fish.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import './Fish.css'
 
 const Fish = (porps) => {
 
     // console.log(porps.fish.id)
 
-    const { id, name, size, FoodValue, Habitats, price, image } = porps.fish;
+    const { id, name, size, FoodValue, Habitats, price, image, details } = porps.fish;
 
     return (
         <div className="col">
             <div className="card h-100">
-                <img src={image} alt="" />
-                <div className="card-body">
+                <img className="bg-fish" src={image} alt="" />
+                <div className="card-body ">
                     <h5 className="card-title">Fish ID: {id}</h5>
                     <h5 className="card-title">Name: {name}</h5>
                     <h5 className="card-title">Size: {size} lbs</h5>
                     <h5 className="card-title">Food Value: {FoodValue}</h5>
                     <h5 className="card-title">Habitats: {Habitats}</h5>
                     <h5 className="card-title">Price: {price}</h5>
-                </div>
-                <div className="card-footer">
-                    <small className="text-muted">ADD TO Cart</small>
+                    <div className="d-flex flex-column align-items-center">
+                        <div className="border rounded-pill border-primary w-75">
+                            <i className="fas fa-angle-double-right"></i>
+                            <a href={details} className="text-muted px-2 py-2 text-decoration-none">   More Details </a>
+                        </div>
+                        <div className="border rounded-pill border-primary w-75 mt-2">
+                            <i className="fas fa-shopping-cart"></i>
+                            <small className="text-muted px-2 py-2 ">   Add TO Cart </small>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
