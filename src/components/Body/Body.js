a93289ebcead916a9e5457580a9bb5d1365e06ec
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-09-26 20:03:01 +0600

    ui done

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -11,12 +11,21 @@ const Body = () => {
     }, [])
 
     return (
-        <div>
-            <h2>this is from body</h2>
-            {
-                fishes.map(fish => <Fish></Fish>)
-            }
-        </div>
+        <div className="container">
+
+            <div className="row">
+                <div className="col-md-9">
+                    <div className="row row-cols-1 row-cols-md-3 g-4">
+                        {
+                            fishes.map(fish => <Fish key={fish.id} fish={fish} ></Fish>)
+                        }
+                    </div>
+                </div>
+                <div className="col-md-3">
+                </div>
+
+            </div>
+        </div >
     );
 };
 
--- src/components/Fish/Fish.js
+++ src/components/Fish/Fish.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
-const Fish = () => {
+const Fish = (porps) => {
+
+    // console.log(porps.fish.id)
+
+    const { id, name, size, FoodValue, Habitats, price, image } = porps.fish;
+
     return (
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-            <div className="col">
-                <div className="card h-100">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Chrome_icon_%28September_2014%29.svg/1200px-Google_Chrome_icon_%28September_2014%29.svg.png"
-                        className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-                            additional content.This card has even longer content than the first to show that equal height
-                            action.</p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </div>
+        <div className="col">
+            <div className="card h-100">
+                <img src={image} alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">Fish ID: {id}</h5>
+                    <h5 className="card-title">Name: {name}</h5>
+                    <h5 className="card-title">Size: {size} lbs</h5>
+                    <h5 className="card-title">Food Value: {FoodValue}</h5>
+                    <h5 className="card-title">Habitats: {Habitats}</h5>
+                    <h5 className="card-title">Price: {price}</h5>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted">ADD TO Cart</small>
                 </div>
             </div>
         </div>
